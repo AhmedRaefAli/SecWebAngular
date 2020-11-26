2e77bc80dbc6d37c44455fa900db7b68341ae0cb
ahmed raef <ahmedraef806@gmail.com>
2020-11-26 12:39:17 +0200

    a7a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PresedentComponent } from './presedent/presedent.component';
 import { HomeComponent } from './home/home.component';
+import { SecComponent } from './sec/sec.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'presedent', component: PresedentComponent },
+  {path: 'sec', component: SecComponent },
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+  }
 
   ngOnInit(): void {
   }
 
+
+  presClicked(): void{
+    this.router.navigate(['/presedent']);
+  }
+
+
+  secClicked(): void{
+    this.router.navigate(['/sec']);
+  }
 }
